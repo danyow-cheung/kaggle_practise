8e04cfee7509d7dfffe972e3027be19119b719e4
danyow-cheung <76671016+danyow-cheung@users.noreply.github.com>
2023-01-09 13:53:16 +0800

    Update yolov1.py

--- paper/YOLO/yolov1.py
+++ paper/YOLO/yolov1.py
@@ -218,3 +218,244 @@ class YOLO_reshape(tf.keras.layers.Layer):
 		
 		return outputs
 
+'''Define the model as bescribed in the original paper'''
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, InputLayer, Dropout, Flatten, Reshape
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, GlobalMaxPooling2D
+from tensorflow.keras.regularizers import l2
+
+lrelu = tf.keras.layers.LeakyReLU(alpha=0.1)
+
+nb_boxes = 1
+grid_w = 7
+grid_h = 7 
+cell_w = 64
+cell_h = 64 
+img_w = grid_w*cell_w
+img_h = grid_h*cell_h
+
+model = Sequential()
+model.add(Conv2D(
+	filters=64,
+	kerel_size = (7,7),
+	strides =(1,1),
+	input_shape = (img_h,img_w,3),
+	padding = 'same',
+	activation = lrelu,
+	kernel_regularizer = l2(5e-4)
+))
+
+model.add(MaxPooling2D(pool_size =(2,2),strides = (2,2),padding='same'))
+
+model.add(Conv2D(filtes=192,
+				kernel_size=(3,3),
+				padding='same',
+				activation=lrelu,
+				kernel_regularizier = l2(5e-4)))
+
+model.add(MaxPooling2D(pool_size=(2,2),strides=(2,2),padding='same'))
+
+model.add(Conv2D(filtes=128,
+				kernel_size=(1,1),
+				padding='same',
+				activation=lrelu,
+				kernel_regularizier = l2(5e-4)))
+
+model.add(Conv2D(filtes=256,
+				kernel_size=(3,3),
+				padding='same',
+				activation=lrelu,
+				kernel_regularizier = l2(5e-4)))
+
+
+model.add(Conv2D(filtes=256,
+				kernel_size=(1,1),
+				padding='same',
+				activation=lrelu,
+				kernel_regularizier = l2(5e-4)))
+
+model.add(Conv2D(filtes=512,
+				kernel_size=(3,3),
+				padding='same',
+				activation=lrelu,
+				kernel_regularizier = l2(5e-4)))
+
+model.add(MaxPooling2D(pool_size=(2,2),strides=(2,2),padding='same'))
+
+
+model.add(Conv2D(filters=256, kernel_size= (1, 1), padding = 'same', activation=lrelu, kernel_regularizer=l2(5e-4)))
+model.add(Conv2D(filters=512, kernel_size= (3, 3), padding = 'same', activation=lrelu, kernel_regularizer=l2(5e-4)))
+model.add(Conv2D(filters=256, kernel_size= (1, 1), padding = 'same', activation=lrelu, kernel_regularizer=l2(5e-4)))
+model.add(Conv2D(filters=512, kernel_size= (3, 3), padding = 'same', activation=lrelu, kernel_regularizer=l2(5e-4)))
+model.add(Conv2D(filters=256, kernel_size= (1, 1), padding = 'same', activation=lrelu, kernel_regularizer=l2(5e-4)))
+model.add(Conv2D(filters=512, kernel_size= (3, 3), padding = 'same', activation=lrelu, kernel_regularizer=l2(5e-4)))
+model.add(Conv2D(filters=256, kernel_size= (1, 1), padding = 'same', activation=lrelu, kernel_regularizer=l2(5e-4)))
+model.add(Conv2D(filters=512, kernel_size= (3, 3), padding = 'same', activation=lrelu, kernel_regularizer=l2(5e-4)))
+model.add(Conv2D(filters=512, kernel_size= (1, 1), padding = 'same', activation=lrelu, kernel_regularizer=l2(5e-4)))
+model.add(Conv2D(filters=1024, kernel_size= (3, 3), padding = 'same', activation=lrelu, kernel_regularizer=l2(5e-4)))
+model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding = 'same'))
+
+model.add(Conv2D(filters=512, kernel_size= (1, 1), padding = 'same', activation=lrelu, kernel_regularizer=l2(5e-4)))
+model.add(Conv2D(filters=1024, kernel_size= (3, 3), padding = 'same', activation=lrelu, kernel_regularizer=l2(5e-4)))
+model.add(Conv2D(filters=512, kernel_size= (1, 1), padding = 'same', activation=lrelu, kernel_regularizer=l2(5e-4)))
+model.add(Conv2D(filters=1024, kernel_size= (3, 3), padding = 'same', activation=lrelu, kernel_regularizer=l2(5e-4)))
+model.add(Conv2D(filters=1024, kernel_size= (3, 3), padding = 'same', activation=lrelu, kernel_regularizer=l2(5e-4)))
+model.add(Conv2D(filters=1024, kernel_size= (3, 3), strides=(2, 2), padding = 'same'))
+
+model.add(Conv2D(filters=1024, kernel_size= (3, 3), activation=lrelu, kernel_regularizer=l2(5e-4)))
+model.add(Conv2D(filters=1024, kernel_size= (3, 3), activation=lrelu, kernel_regularizer=l2(5e-4)))
+
+
+
+model.add(Flatten())
+model.add(Dense(512))
+model.add(Dense(1024))
+model.add(Dropout(0.5))
+model.add(Dense(1470,activation='sigmoid'))
+model.add(YOLO_reshape(target_shape=(7,7,30)))
+model.summary()
+
+
+'''Define a custom learning rate scheduler'''
+class CustomLearningRateScheduler(keras.callbacks.Callback):
+	'''Learning rate scheduler which sets the learning rate according to schedule
+	Arguments:
+		schedule: a function that takes an epoch index (integer,indexed from 0) and current learning rate
+				  as a inputs and returns a new learning rate as output(float)
+		
+	'''
+
+	def __init__(self,schedule):
+		super(CustomLearningRateScheduler).__init___()	
+		self.schedule = schedule 
+	
+	def on_epoch_begin(self,epoch,logs=None):
+		if not hasattr(self.model.optimizer,"lr"):
+			raise ValueError("Optimizer must have a lr attribute")
+		# get the current learning rate from model's optimizer
+		lr = float(tf.keras.backend.get_value(self.model.optimizer.learning_rate))
+		# call schedule function to get the scheduled learning rate
+		scheduled_lr = self.schedule(epoch,lr)
+
+		# set the value back to the optimizer before this epoch starts
+		tf.keras.backend.set_value(self.model.optimizer.lr,scheduled_lr)
+		print("\nEpoch %05d: Learning rate is %6.4f." % (epoch, scheduled_lr))
+
+LR_SCHEDULE=[
+	# (epoch to start,learning rate) tuples
+	(0,0.01),
+	(75,0.001),
+	(105,0.0001),
+]
+
+def lr_schedule(epoch,lr):
+	'''Helper function to retrieve the schduled learning rate based on epoch'''
+	if epoch<LR_SCHEDULE[0][0] or epoch>LR_SCHEDULE[-1][0]:
+		return lr 
+	for i in range(len(LR_SCHEDULE)):
+		if epoch == LR_SCHEDULE[i][0]:
+			return LR_SCHEDULE[i][1]
+	return lr 
+
+'''Define the loss function
+'''
+def xywh2minmax(xy,wh):
+	xy_min = xy - wh/2 
+	xy_max = xy + wh/2 
+	return xy_min,xy_max
+
+def iou(pred_mins,pred_maxes,true_mins,true_maxes):
+	intersect_mins = K.maximum(pred_mins,true_mins)
+	intersect_maxes = K.minimum(pred_maxes,true_maxes)
+
+	intersect_wh = K.maximum(intersect_maxes - intersect_mins,0.)
+	intersect_areas = intersect_wh[...,0]*intersect_wh[...,1]
+
+	pred_wh = pred_maxes - pred_mins
+	true_wh = true_maxes - true_mins
+	pred_areas = pred_wh[...,0] * pred_wh[...,1]
+	true_areas = true_wh[...,0] * true_wh[...,1]
+
+	union_areas = pred_areas + true_areas - intersect_areas
+	iou_scores = intersect_areas / union_areas
+	return iou_scores
+
+def yolo_head(feats):
+	# Dynamic inplementation of conv dims for fully convolutional model
+	conv_dims = K.shape(feats)[1:3] # assuming channels last
+	# in YOLO the height index is the inner most iteration
+	conv_height_index = K.arange(0,stop=conv_dims[0])
+	conv_width_index = K.arange(0,stop=conv_dims[1])
+	
+	# TODO:Repeat_elements and tf.split doesn't support dynamic splits
+	# conv_width_index = K.repeat_elements(conv_width_index,conv_dims[1],axis=0)
+
+	conv_width_index = K.tile(K.expand_dims(conv_width_index,0),[conv_dims[0],1])
+	conv_width_index = K.flatten(K.transpose(conv_width_index))
+
+	conv_index = K.transpose(K.stack([conv_height_index,conv_width_index]))
+	conv_index = K.reshape(conv_index, [1, conv_dims[0], conv_dims[1], 1, 2])
+	
+	conv_index = K.cast(conv_index, K.dtype(feats))
+
+	conv_dims = K.cast(K.reshape(conv_dims,[1,1,1,1,2]),K.dtype(feats))
+
+	box_xy = (feats[...,2]+conv_index) / conv_dims*448
+	box_wh = feats[...,2:4] * 448 
+	return box_xy,box_wh
+
+
+def yolo_loss(y_true,y_pred):
+	label_class = y_true[...,:20]
+	label_box = y_true[...,20:24]
+	response_mask = y_true[...,24]
+	response_mask = K.expand_dims(response_mask)
+	
+	predict_class = y_pred[...,:20]
+	predict_trust = y_pred[...,20:22]
+	predict_box = y_pred[...,22:]
+
+	_label_box  = K.reshape(label_box,[-1,7,7,1,4])
+	_predict_box =  K.reshape(predict_box,[-1,7,7,2,4])
+
+	label_xy,label_wh = yolo_head(_label_box)
+
+	label_xy = K.expand_dims(label_xy,3)
+	label_wh = K.expand_dims(label_wh,3)
+	label_xy_min,label_xy_max = xywh2minmax(label_xy,label_wh,)
+
+	predict_xy,predict_wh = yolo_head(_predict_box)
+	
+	predict_xy = K.expand_dims(predict_xy,4)
+	predict_wh = K.expand_dims(predict_wh,4)
+	predict_xy_min,predict_xy_max = xywh2minmax(predict_xy,predict_wh)
+
+	iou_scores = iou(predict_xy_min,predict_xy_max,label_xy_min,label_xy_max)
+	best_ious  = K.max(iou_scores,axis=4)
+	best_box = K.max(best_ious,axis=3,keepdims=True)
+	box_mask = K.cast(best_ious>=best_box,K.dtype(best_ious))
+
+	no_object_loss = 0.5*(1-box_mask*response_mask)*K.square(0-predict_trust)
+	object_loss = box_mask * response_mask *K.square(1-predict_trust)
+	confidence_loss = no_object_loss+ object_loss
+	confidence_loss K.sum(confidence_loss)
+
+	class_loss = response_mask *K.square(label_class - predict_class)
+	class_loss = K.sum(class_loss)
+
+	_label_box = K.reshape(label_box,[-1,7,7,1,4])
+	_predict_box = K.reshape(predict_box,[-1,7,7,2,4])
+
+	label_xy,label_wh = yolo_head(_label_box)
+	predict_xy ,predict_wh  = yolo_head(_predict_box)
+
+	box_mask = K.expand_dims(box_mask)
+	response_mask = K.expand_dims(response_mask)
+
+	box_loss = 5*box_mask*response_mask*K.square((label_xy-predict_xy)/448)
+	box_loss+= 5*box_mask * response_mask *K.square((K.sqrt(label_wh) - K.sqrt(predict_wh))/448)
+	box_loss = K.sum(box_loss)
+
+	loss = confidence_loss+class_loss + box_loss
+	return loss 
+	
