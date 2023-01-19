430c783c9c51fe19111c579898c8fcc448ac5acc
danyow-cheung <76671016+danyow-cheung@users.noreply.github.com>
2023-01-19 10:17:35 +0800

    1.19

---
+++ paper/Faster-RCNN/fasterrcnn.py
@@ -0,0 +1,10 @@
+'''
+pytorch
+https://towardsdatascience.com/understanding-and-implementing-faster-r-cnn-a-step-by-step-guide-11acfff216b0'''
+
+
+'''
+tensorflow
+https://pub.towardsai.net/training-faster-r-cnn-using-tensorflow-object-detection-api-with-a-custom-dataset-88dd525666fd
+'''
+
