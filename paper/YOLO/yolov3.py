ab2b0aa531ac11ab0e79d9f223ada15d407df866
danyow-cheung <76671016+danyow-cheung@users.noreply.github.com>
2023-02-06 14:20:37 +0800

    2.6

--- paper/YOLO/yolov3.py
+++ paper/YOLO/yolov3.py
@@ -140,7 +140,46 @@ def load_weights(var_list,weights_file):
 		var1 = var_list[i]
 		var2 = var_list[i+1]
 		# 2.4 to be continue 
-		
+		# doing something only if we process conv layer 
+		if 'Conv' in var1.name.split('/')[-2]:
+			# check type of next layer 
+			if 'BatchNorm' in var2.name.split('/')[-2]:
+				# load batch norm params 
+				gamma,beta,mean,var = var_list[i+1:i+5]
+				batch_norm_vars = [beta,gamma,mean,var]
+				for var in batch_norm_vars:
+					shape = var.shape.as_list()
+					num_params = np.prod(shape)
+					var_weights = weights[ptr:ptr+num_params].reshape(shape)
+					ptr+= num_params
+					assign_ops.append(tf.assign(var,var_weights,validate_type=True))
+
+				# we move the pointer by 4 ,because we loaded 4 variables 
+				i+=4 
+			elif 'Conv' in var2.name.split('/')[-2]:
+				# load biases 
+				bias = var2 
+				bias_shape = bias.shape.as_list()
+				bias_params = np.prod(bias_shape)
+				bias_weights  = weights[bias_shape]
+				ptr += bias_params
+				assign_ops.append(tf.assign(bias,bias_weights,validate_type=True))
+				# we loaded 2 variables 
+				i+=1 
+
+			# we can load weights of conv layer 
+			shape = var1.shape.as_list()
+			num_params = np.prod(shape)
+
+			var_weights = weights[ptr:ptr+num_params].reshape((shape[3],shape[2],shape[0],shape[1]))
+	
+			# remember to transpose to column-major 
+			var_weights = np.transpose(var_weights,(2,3,1,0))
+			ptr+=num_params
+			assign_ops.append(tf.assign(var1,var_weights,validate_type=True))
+			i+=1 
+	return assign_ops 
+
 
 _BATCH_NORM_DECAY = 0.9 
 _BATCH_NORM_EPSILON =1e-5 
@@ -325,6 +364,94 @@ def _upsample(inputs,out_shape,data_format='NCHW'):
 
 	inputs = tf.identity(inputs,name='upsampled')
 	return inputs 
+
+
+with tf.variable_scope('model'):
+	model = yolo_v3(inputs,80)
+model_vars = tf.global_variables(scope='model')
+assign_ops = load_weights(model_vars,'yolov3.weights')
+sees = tf.Session()
+sees.run(assign_ops)
+
+
+'''Implementation of post-processing algorithms'''
+def detections_boxes(detections):
+	center_x ,center_y,width,height ,attrs = tf.split(detections,[1,1,1,1,-1],axis=-1)
+	w2 = width/2 
+	h2 = height/2
+	x0 = center_x - w2 
+	y0 = center_y- h2 
+	x1 = center_x+w2 
+	y1 = center_y+h2 
+
+	boxes = tf.concat([x0,y0,x1,y1],axis=-1)
+	detections = tf.concat([boxes,attrs],axis=-1)
+	return detections
+
+'''NMS algorithm'''
+def _iou(box1,box2):
+	b1_x0,b1_y0,b1_x1,b1_y1 = box1
+	b2_x0,b2_y0,b2_x1,b2_y1 = box2 
+
+	int_X0 = max(b1_x0,b2_x0)
+	int_y0 = max(b1_y0,b2_y0)
+	int_x1 = max(b1_x1,b2_x1)
+	int_y1 = max(b1_y1,b2_y1)
+
+	int_area = (int_x1-int_x0) *(int_y1 -int_y0)
+	b1_area = (b1_x1-b1_x0)*(b1_y1-b1_y0)
+	b2_area = (b2_x1 - b2_x0)*(b2_y1-b2_y0)
+
+	iou = int_area/(b1_area+b2_area -int_area + 1e-05)
+	return iou
+
+def non_max_supreesion(predictions_with_boxes,confidence_threshold,iou_threshold=0.4):
+	'''applies non-max supression to prediction boxed
+	:params predictions_with_boxes:	3D numpy array first 4 values in3rd dimension are bbox attrs,5th is confidence
+	:params confidence_threshold:the threshold for deciding if prediction is valid 
+	:params iou_threshold :the threshold for detecting if prediction is valid 
+
+	:params dict:the threshold for detecting if two boxes overlap 
+	:return dict:class->[(box,score)]	
+	'''
+
+	conf_mask = np.expand_dims((predictions_with_boxes[:,:,4]>confidence_threshold),-1)
+	confidence = predictions_with_boxes*conf_mask
+
+	resutlt = {}
+	for i,image_pred in enumerate(predictions):
+		shape = image_pred.shape
+		non_zero_idxs = np.nonzero(image_pred)
+		image_pred = image_pred[non_zero_idxs]
+		image_pred = image_pred.reshape(-1,shape[-1])
+
+		bbox_attrs = image_pred[:,:,5]
+		classes = image_pred[:,5:]
+		classes = np.argmax(classes,axis=-1)
+
+		unique_classes = list(set(classes.reshape(-1)))
+
+		for cls in unique_classes:
+			cls_mask = classes == cls 
+			cls_boxes = bbox_attrs[np.nonzero(image_pred)]
+			cls_boxes = cls_boxes[cls_boxes[:,-1].argsort()[::-1]]
+			cls_scores = cls_boxes[:,:,-1]
+			cls_boxes = cls_boxes[:,:,-1]
+
+			while len(cls_boxes)>0:
+				box = cls_boxes[0]
+				score = cls_scores[0]
+				if not cls in result:
+					resutlt[cls] = []
+				resutlt[cls].append((box,score))
+				cls_boxes = cls_boxes[1:]
+				ious = np.array(_iou(box,x) for x in cls_boxes)
+				iou_mask = iouts<iou_threshold
+				cls_boxes = cls_boxes[np.nonzero(iou_mask)]
+				cls_scores = cls_scores[np.nonzero(iou_mask)]
+	return result 
+	# 2.6 end 
+	
 
 
 
